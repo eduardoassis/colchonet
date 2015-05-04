d07a09d3e137404c997415a65294cacba11ea664
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2015-05-04 20:49:55 -0300

    Alterando a forma de realizar validação

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,8 @@ class User < ActiveRecord::Base
 	validates_confirmation_of :password
 	validates_length_of :bio, minimum: 30, allow_blank: false
 
-	validate :email_format
+	validates :email, presence: true,
+					  format: { with: EMAIL_REGEXP },
+					  uniqueness: true
 
-	private
-
-	def email_format
-		errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
-	end
 end
